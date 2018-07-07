b06c25ff0235d7510f7f2e30a66ac7cc9e3ec2fc
tingsoon <angts8@hotmail.com>
2018-07-08 00:46:19 +0800

    edited home page.

--- views/home.jsx
+++ views/home.jsx
@@ -44,54 +44,30 @@ class Home extends React.Component {
 				  		</div>
 				  		</a>
 
-				  		<a href="http://localhost:3000/hawkers/East">
+				  		<a href="http://localhost:3000/hawkers/east">
 				  		<div className="location" id="east">
 				  			<span className="location-name">East</span>
 				  		</div>
 				  		</a>
 				  		</div>
 
-				      	<a className="Central" href="http://localhost:3000/hawkers/central">
-				      		<img className="imgResize " src={"https://magazine.foodpanda.sg/wp-content/uploads/sites/2/2016/08/Best-Satay-Header.jpg"} />
-				      	</a>
-				      	<a className="West" href="http://localhost:3000/hawkers/west">
-				      		<img className="imgResize " src={"http://www.shape.com.sg/wp-content/uploads/2017/08/How-to-make-Singapore-hawker-food-healthier-640x426.jpg"} />
-				    	</a>
+				  		<div className="row-2">
+				  		<a href="http://localhost:3000/hawkers/central">
+				  		<div className="location" id="central">
+				  			<span className="location-name">Central</span>
+				  		</div>
+				  		</a>
+
+				  		<a href="http://localhost:3000/hawkers/west">
+				  		<div className="location" id="west">
+				  			<span className="location-name">West</span>
+				  		</div>
+				  		</a>
+				  		</div>
+
 				    </div>
 				</div>
 	    		<hr className="my-4" />
-	    		<div>
-		    		<form className="searchNorth" method="GET" action='/hawkers/north'>
-	                <input type="hidden" name="searchNorth" />
-	                <div className="buttonDiv">
-	                	<input className="searchNorth" type="submit" value="North" />
-		            </div>
-		            </form>
-       		   </div>
-	    		<div>
-		    		<form className="searchEast" method="GET" action='/hawkers/east'>
-	                <input type="hidden" name="searchEast" />
-	                <div className="buttonDiv">
-	                	<input className="searchEast" type="submit" value="East" />
-		            </div>
-		            </form>
-       		   </div>
-	    		<div>
-		    		<form className="searchCentral" method="GET" action='/hawkers/central'>
-	                <input type="hidden" name="searchCentral" />
-	                <div className="buttonDiv">
-	                	<input className="searchCentral" type="submit" value="Central" />
-		            </div>
-		            </form>
-       		   </div>
-	    		<div>
-		    		<form className="searchWest" method="GET" action='/hawkers/west'>
-	                <input type="hidden" name="searchWest" />
-	                <div className="buttonDiv">
-	                	<input className="searchWest" type="submit" value="West" />
-		            </div>
-		            </form>
-       		   </div>
        		   </div> 
        		   </div>    		   
 	    	</div>
