d7310bb2478b1e0bc3d4baeb239ed6897468d5dd
tingsoon <angts8@hotmail.com>
2018-07-12 16:55:14 +0800

    added heroku stuff

--- db.js
+++ db.js
@@ -14,12 +14,35 @@ const users = require('./models/user');
 const hawkers = require('./models/hawkers');
 const reviews = require('./models/review');
 
+const url = require('url');
+
+//check to see if we have this heroku environment variable
+if( process.env.DATABASE_URL ){
+
+  //we need to take apart the url so we can set the appropriate configs
+
+  const params = url.parse(process.env.DATABASE_URL);
+  const auth = params.auth.split(':');
+
+  //make the configs object
+  var configs = {
+    user: auth[0],
+    password: auth[1],
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split('/')[1],
+    ssl: true
+  };
+
+} else {
+
  const configs = {
   user: 'angtingsoon',
   host: '127.0.0.1',
   database: 'hawkerlah',
   port: 5432,
 };
+// }
 
 const pool = new pg.Pool(configs);
 
